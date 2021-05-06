6b97f1f1a1683fd92dfc5e4d6d895647a9fb7957
Gavin <gavinmtannaka@hotmail.com>
2021-05-06 22:14:29 +0700

    initial commit

---
+++ src/handler.js
@@ -0,0 +1,120 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+    const { 
+        name, 
+        year, 
+        author, 
+        summary, 
+        publisher, 
+        pageCount,
+        readPage,
+        reading
+    } = request.payload;
+
+    const id = nanoid(16);
+    const finished = pageCount === readPage;
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+
+    const newBook = {
+        name, 
+        year, 
+        author, 
+        summary, 
+        publisher, 
+        pageCount,
+        readPage,
+        reading,
+        id,
+        finished,
+        insertedAt,
+        updatedAt
+    };
+
+    const checkName = newBook.name === undefined;
+    const checkReadPage = newBook.pageCount < newBook.readPage;
+    
+    if (checkName) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. Mohon isi nama buku',
+        });
+        response.code(400);
+        return response;
+    }
+
+    if (checkReadPage) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+        });
+        response.code(400);
+        return response;
+    }
+
+    books.push(newBook);
+
+    const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+    if (isSuccess) {
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil ditambahkan',
+            data: {
+                bookId: id,
+            },
+        });
+        response.code(201);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku gagal ditambahkan',
+    });
+    response.code(500);
+    return response;
+};
+
+const getAllBooksHandler = () => ({
+    status: 'success',
+    data: 
+    {
+        books: books.map((book) => ({
+        id: book.id,
+        name: book.name,
+        publisher: book.publisher,
+        })),
+    }
+});
+
+const getBookByIdHandler = (request, h) => {
+    const { id } = request.params;
+    const book = books.filter((b) => b.id === id)[0];
+
+    if (book !== undefined) {
+        const response = h.response({
+            status: 'success',
+            data: {
+                book,
+            },
+        });
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+}
+
+module.exports = {
+    addBookHandler,
+    getAllBooksHandler,
+    getBookByIdHandler
+};
---
+++ src/server.js
@@ -0,0 +1,21 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.21.235',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server running on ${server.info.uri} on port ${server.info.port}`);
+};
+
+init();
